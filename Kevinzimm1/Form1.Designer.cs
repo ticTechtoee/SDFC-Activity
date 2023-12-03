c556e4fd38493314910671432b2f0f16650ee46d
Farhan Basheer <farhanfb6@outlook.com>
2023-12-03 14:02:50 +0500

    Initial Commit

--- Kevinzimm1/Form1.Designer.cs
+++ Kevinzimm1/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Kevinzimm1
 {
-    partial class Form1
+    partial class sdfcactivityFrm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,249 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.BtnLoadFile = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.CmbBoxOppNm = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.CmbCategory = new System.Windows.Forms.ComboBox();
+            this.CmbActivity = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtSbj = new System.Windows.Forms.RichTextBox();
+            this.txtDesc = new System.Windows.Forms.RichTextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.chkDemo = new System.Windows.Forms.CheckBox();
+            this.chkEnt = new System.Windows.Forms.CheckBox();
+            this.BtnOk = new System.Windows.Forms.Button();
+            this.BtnClose = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.LblNULL = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BtnLoadFile
+            // 
+            this.BtnLoadFile.Location = new System.Drawing.Point(13, 13);
+            this.BtnLoadFile.Name = "BtnLoadFile";
+            this.BtnLoadFile.Size = new System.Drawing.Size(75, 23);
+            this.BtnLoadFile.TabIndex = 0;
+            this.BtnLoadFile.Text = "Load File";
+            this.BtnLoadFile.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Opportunity Name";
+            // 
+            // CmbBoxOppNm
+            // 
+            this.CmbBoxOppNm.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.CmbBoxOppNm.FormattingEnabled = true;
+            this.CmbBoxOppNm.Location = new System.Drawing.Point(15, 84);
+            this.CmbBoxOppNm.Name = "CmbBoxOppNm";
+            this.CmbBoxOppNm.Size = new System.Drawing.Size(364, 21);
+            this.CmbBoxOppNm.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 131);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Date";
+            // 
+            // dateTimePicker
+            // 
+            this.dateTimePicker.Location = new System.Drawing.Point(14, 149);
+            this.dateTimePicker.Name = "dateTimePicker";
+            this.dateTimePicker.Size = new System.Drawing.Size(364, 20);
+            this.dateTimePicker.TabIndex = 4;
+            // 
+            // CmbCategory
+            // 
+            this.CmbCategory.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.CmbCategory.FormattingEnabled = true;
+            this.CmbCategory.Location = new System.Drawing.Point(16, 209);
+            this.CmbCategory.Name = "CmbCategory";
+            this.CmbCategory.Size = new System.Drawing.Size(364, 21);
+            this.CmbCategory.TabIndex = 5;
+            // 
+            // CmbActivity
+            // 
+            this.CmbActivity.FormattingEnabled = true;
+            this.CmbActivity.Location = new System.Drawing.Point(16, 269);
+            this.CmbActivity.Name = "CmbActivity";
+            this.CmbActivity.Size = new System.Drawing.Size(364, 21);
+            this.CmbActivity.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 193);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Category";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 253);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Activity";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 314);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(43, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Subject";
+            // 
+            // txtSbj
+            // 
+            this.txtSbj.Location = new System.Drawing.Point(13, 331);
+            this.txtSbj.Name = "txtSbj";
+            this.txtSbj.Size = new System.Drawing.Size(367, 96);
+            this.txtSbj.TabIndex = 10;
+            this.txtSbj.Text = "";
+            // 
+            // txtDesc
+            // 
+            this.txtDesc.Location = new System.Drawing.Point(12, 459);
+            this.txtDesc.Name = "txtDesc";
+            this.txtDesc.Size = new System.Drawing.Size(367, 96);
+            this.txtDesc.TabIndex = 12;
+            this.txtDesc.Text = "";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(11, 442);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(60, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Description";
+            // 
+            // chkDemo
+            // 
+            this.chkDemo.AutoSize = true;
+            this.chkDemo.Location = new System.Drawing.Point(93, 578);
+            this.chkDemo.Name = "chkDemo";
+            this.chkDemo.Size = new System.Drawing.Size(63, 17);
+            this.chkDemo.TabIndex = 13;
+            this.chkDemo.Text = "Demo ?";
+            this.chkDemo.UseVisualStyleBackColor = true;
+            // 
+            // chkEnt
+            // 
+            this.chkEnt.AutoSize = true;
+            this.chkEnt.Location = new System.Drawing.Point(235, 578);
+            this.chkEnt.Name = "chkEnt";
+            this.chkEnt.Size = new System.Drawing.Size(51, 17);
+            this.chkEnt.TabIndex = 14;
+            this.chkEnt.Text = "Ent ?";
+            this.chkEnt.UseVisualStyleBackColor = true;
+            // 
+            // BtnOk
+            // 
+            this.BtnOk.Location = new System.Drawing.Point(16, 627);
+            this.BtnOk.Name = "BtnOk";
+            this.BtnOk.Size = new System.Drawing.Size(166, 33);
+            this.BtnOk.TabIndex = 15;
+            this.BtnOk.Text = "OK";
+            this.BtnOk.UseVisualStyleBackColor = true;
+            // 
+            // BtnClose
+            // 
+            this.BtnClose.Location = new System.Drawing.Point(214, 627);
+            this.BtnClose.Name = "BtnClose";
+            this.BtnClose.Size = new System.Drawing.Size(166, 33);
+            this.BtnClose.TabIndex = 16;
+            this.BtnClose.Text = "CLOSE";
+            this.BtnClose.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(9, 690);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(57, 13);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Last Entry:";
+            // 
+            // LblNULL
+            // 
+            this.LblNULL.AutoSize = true;
+            this.LblNULL.Location = new System.Drawing.Point(79, 690);
+            this.LblNULL.Name = "LblNULL";
+            this.LblNULL.Size = new System.Drawing.Size(35, 13);
+            this.LblNULL.TabIndex = 18;
+            this.LblNULL.Text = "NULL";
+            // 
+            // sdfcactivityFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(392, 712);
+            this.Controls.Add(this.LblNULL);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.BtnClose);
+            this.Controls.Add(this.BtnOk);
+            this.Controls.Add(this.chkEnt);
+            this.Controls.Add(this.chkDemo);
+            this.Controls.Add(this.txtDesc);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtSbj);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.CmbActivity);
+            this.Controls.Add(this.CmbCategory);
+            this.Controls.Add(this.dateTimePicker);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.CmbBoxOppNm);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BtnLoadFile);
+            this.MaximizeBox = false;
+            this.Name = "sdfcactivityFrm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SDFC Activity";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button BtnLoadFile;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox CmbBoxOppNm;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker;
+        private System.Windows.Forms.ComboBox CmbCategory;
+        private System.Windows.Forms.ComboBox CmbActivity;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RichTextBox txtSbj;
+        private System.Windows.Forms.RichTextBox txtDesc;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.CheckBox chkDemo;
+        private System.Windows.Forms.CheckBox chkEnt;
+        private System.Windows.Forms.Button BtnOk;
+        private System.Windows.Forms.Button BtnClose;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label LblNULL;
     }
 }
 
--- Kevinzimm1/Program.cs
+++ Kevinzimm1/Program.cs
@@ -16,7 +16,7 @@ namespace Kevinzimm1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new sdfcactivityFrm());
         }
     }
 }
